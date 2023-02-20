7b5b5e37f4c556bf235d524b3fbe21edf15f4da8
jo-duchan <jo_duchan@icloud.com>
2023-02-20 23:04:29 +0900

    section3 - 36

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
-import "App.css";
+import ExpenseItem from "components/Expenseltem";
 
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <div className="App">
+      <h2>Let's get started!</h2>
+      <ExpenseItem />
+    </div>
+  );
 }
 
 export default App;
