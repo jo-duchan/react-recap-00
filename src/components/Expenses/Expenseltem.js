b9703d7d8f5832b1aebedb958b581f9608aadf96
jo-duchan <jo_duchan@icloud.com>
2023-02-25 01:20:02 +0900

    section4-52

--- src/components/Expenses/Expenseltem.js
+++ src/components/Expenses/Expenseltem.js
@@ -1,15 +1,25 @@
-import "components/Expenses/ExpenseItem.css";
+import React, { useState } from "react";
+
 import ExpenseDate from "components/Expenses/ExpenseDate";
 import Card from "components/UI/Card";
+import "components/Expenses/ExpenseItem.css";
 
 function ExpenseItem({ title, amount, date }) {
+  const [itemTitle, setItemTitle] = useState(title);
+
+  const clickHandler = () => {
+    console.log("Clicked");
+    setItemTitle("Updated!");
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{itemTitle}</h2>
         <div className="expense-item__price">${amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
