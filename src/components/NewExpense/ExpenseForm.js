5f461f4f51185e8cf09eb0ff708e5e51fa2c650f
jo-duchan <jo_duchan@icloud.com>
2023-02-27 18:26:08 +0900

    section4-60

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,14 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "components/NewExpense/ExpenseForm.css";
 
 function ExpenseForm() {
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
+
   const titleChangeHandler = (e) => {
-    console.log(e.target.value);
+    setEnteredTitle(e.target.value);
+  };
+
+  const amountChangeHandler = (e) => {
+    setEnteredAmount(e.target.value);
+  };
+
+  const dateChangeHandeler = (e) => {
+    setEnteredDate(e.target.value);
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate),
+    };
+
+    console.log(expenseData);
   };
 
   return (
-    <from>
+    <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
@@ -16,17 +40,27 @@ function ExpenseForm() {
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" min="0.01" step="0.01" />
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            onChange={amountChangeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min="2019-01-01" max="2022-12-31" />
+          <input
+            type="date"
+            min="2019-01-01"
+            max="2022-12-31"
+            onChange={dateChangeHandeler}
+          />
         </div>
       </div>
       <div className="new-expense__actions">
         <button type="submit">Add Expense</button>
       </div>
-    </from>
+    </form>
   );
 }
 
